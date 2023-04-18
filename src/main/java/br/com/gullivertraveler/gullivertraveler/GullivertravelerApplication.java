261abcfab6c93c82ff4265d442063a2162455f69
Luciana Pereira dos Santos Souza <l.dos.santos.souza@avanade.com>
2023-04-17 23:32:38 -0300

    first commit

---
+++ src/main/java/br/com/gullivertraveler/gullivertraveler/GullivertravelerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gullivertraveler.gullivertraveler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GullivertravelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GullivertravelerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gullivertraveler/gullivertraveler/GullivertravelerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gullivertraveler.gullivertraveler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GullivertravelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
